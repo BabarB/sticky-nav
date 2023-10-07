c510a6860eb9a7c1b6240f6a7bc38038bc9ccb5b
BabarB <barbara.bekefi@gmail.com>
2023-10-07 15:04:55 +0200

    write myfunction logic

---
+++ script.js
@@ -0,0 +1,17 @@
+const navbar = document.getElementById ("navbar");
+const sticky = navbar.offsetTop;
+
+
+function myFunction() {
+    //compare havbar distance from top with the vertical scroll value
+    if(window.scrollY>=sticky){
+        //Add .stickyCssClass to navbar
+        navbar.classList.add ("stickyCssClass");
+        //avoid jump scroll of content by adding a margin top. 
+        //Index always to be defined
+        document.getElementsByClassName("content")[0].style.marginTop = "60px";
+    }
+    else{
+        navbar.classList.remove("stickyCssClass");
+    }
+}
